fd2b48af9efad7de1b9074d60b91fbe2c39ffde0
nickmly <nickmly@gmail.com>
2020-02-13 22:57:29 -0500

    Education image updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8664ca8e44be17f8b103f834586ebf9d.js"
+  "/precache-manifest.8349973ca15c746f2b840e955c44b556.js"
 );
 
 self.addEventListener('message', (event) => {
