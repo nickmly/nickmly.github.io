267db6659b1fe36a11a62462695a4d75f52b95bd
nickmly <nickmly@gmail.com>
2020-02-13 19:26:44 -0500

    React version

---
+++ precache-manifest.fb49596f0c623665898b0565c4f68c2a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b294be3eb778967f73989a6bace3075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a136559daaec6b4cfbda",
+    "url": "/static/css/2.3cadc252.chunk.css"
+  },
+  {
+    "revision": "65ab287c2af462b39997",
+    "url": "/static/css/main.6f6a1184.chunk.css"
+  },
+  {
+    "revision": "a136559daaec6b4cfbda",
+    "url": "/static/js/2.89662cb8.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.89662cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ab287c2af462b39997",
+    "url": "/static/js/main.db761f4f.chunk.js"
+  },
+  {
+    "revision": "77d451e7038c8864e2b2",
+    "url": "/static/js/runtime-main.6e48ae5f.js"
+  },
+  {
+    "revision": "e18ee6e97e2974f59ce09dedaa1a1180",
+    "url": "/static/media/ar_1.e18ee6e9.jpg"
+  },
+  {
+    "revision": "b5487938352bdbd6fefaa17a93965cfd",
+    "url": "/static/media/asp_1.b5487938.jpg"
+  },
+  {
+    "revision": "59061fab4224668a83fa05a45245d090",
+    "url": "/static/media/ods.59061fab.jpg"
+  },
+  {
+    "revision": "0ab96ef39a49b4e2dc12057eba8a4908",
+    "url": "/static/media/ontario.0ab96ef3.png"
+  },
+  {
+    "revision": "435823d69097ff53ae42afea5b00bd47",
+    "url": "/static/media/profilePic.435823d6.jpg"
+  },
+  {
+    "revision": "a96dda810765added07a7bbae673a375",
+    "url": "/static/media/react_1.a96dda81.jpg"
+  },
+  {
+    "revision": "cd079e73c31670db9475e59d861dc275",
+    "url": "/static/media/recession_1.cd079e73.jpg"
+  },
+  {
+    "revision": "f6b14c37b30a2733acdc597968b7b0a4",
+    "url": "/static/media/social_1.f6b14c37.jpg"
+  },
+  {
+    "revision": "67921ab1deda7a1cb808dc85b56adbe1",
+    "url": "/static/media/tojam_1.67921ab1.jpg"
+  },
+  {
+    "revision": "fb90b3cb58f07da9c16c85a491d96ffa",
+    "url": "/static/media/travel_1.fb90b3cb.jpg"
+  }
+]);
