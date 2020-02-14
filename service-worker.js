c1e7c2526c8c4ecfcdf66e9451b2b8ab44f156ff
nickmly <nickmly@gmail.com>
2020-02-14 08:46:17 -0500

    Media query for timeline items

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9884b2ee3bfe1865d917a7394c1d9c5.js"
+  "/precache-manifest.2d74ddf21cf38ee3f95161f52b0a8e45.js"
 );
 
 self.addEventListener('message', (event) => {
